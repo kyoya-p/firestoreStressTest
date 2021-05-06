921ff510fb74cf130c617c58496a0ad16936f609
kyoya <kyoya.p@gmail.com>
2021-05-06 12:56:00 +0900

    n

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,27 @@
+/* eslint-disable */
 import * as functions from "firebase-functions";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const addMessage = functions.https.onRequest(async (req, res) => {
+  const limit: number = parseInt(req.query.limit as string);
+  var count = 0;
+  var start = Date.now();
+  var end = start + 10 * 1000;
+  for (var i = 0; Date.now() < end; i = (i + 1) % limit) {
+    var result = await report(`${i}`)
+    if (result != null) count++;
+  }
+  res.json({ result: `Message: ${count}: items added. ${start}~${end}` });
 });
+
+function report(id: String) {
+  return admin.firestore().collection('messages').add(
+    {
+      "count": id,
+      "now": `${Date()}`,
+      "now2": `${Date.now()}`,
+    }
+  );
+}
