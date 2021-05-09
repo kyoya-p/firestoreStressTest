8ea422624674ea4d449a8b3fe7705a4bc3cb6651
kyoya <kyoya.p@gmail.com>
2021-05-10 08:07:11 +0900

    n

--- TestClientJvmKt/src/main/kotlin/common.kt
+++ TestClientJvmKt/src/main/kotlin/common.kt
@@ -6,22 +6,10 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
-import kotlinx.datetime.Clock
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
-import kotlinx.serialization.Serializable
-import org.threeten.bp.DateTimeUtils.toLocalDateTime
-import kotlin.time.ExperimentalTime
+
 
 // set GOOGLE_APPLICATION_CREDENTIALS=path/to/firestoreCredetialFile.json
-val firestore = FirestoreOptions.getDefaultInstance().service!!
+val db = FirestoreOptions.getDefaultInstance().service!!
 
 // https://jp.ktor.work/clients/index.html
 val httpClient = HttpClient(CIO) {
